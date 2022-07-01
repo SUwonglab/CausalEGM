34ba021b20249c010bf53ffe84bdb72f995c8fe2
kimmo1019 <liuqiao@buaa.edu.cn>
2022-07-01 10:54:18 -0700

    script for running causalEGM

---
+++ src/run_causalEGM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mem=10G
+#SBATCH -p whwong
+#SBATCH -n 1
+#SBATCH -G 1
+#SBATCH --time=2-00:00
+#SBATCH -e error.txt
+#SBATCH -o output.txt
+
+ml load cuda/11.2.0
+ml load cudnn/8.1.1.33
+ml python/3.9.0
+
+#time /share/software/user/open/python/3.9.0/bin/python3 main.py -c configs/sim_linear.yaml
+#time /share/software/user/open/python/3.9.0/bin/python3 main.py -c configs/sim_quadratic.yaml
+time /share/software/user/open/python/3.9.0/bin/python3 main.py -c configs/sim_Hirano_Imbens.yaml
