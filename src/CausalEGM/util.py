9039b56d3f90fb5abf9126d9461f11bcd6eaee43
kimmo1019 <liuqiao@buaa.edu.cn>
2022-11-30 23:36:11 -0800

    add Assump_valid_sampler

--- src/CausalEGM/util.py
+++ src/CausalEGM/util.py
@@ -207,81 +207,40 @@ class Gaussian_sampler(object):
     def load_all(self):
         return self.X, self.Y
 
-#sample continuous (Gaussian) and discrete (Catagory) latent variables together
-class Mixture_sampler(object):
-    def __init__(self, nb_classes, N, dim, sd, scale=1):
-        self.nb_classes = nb_classes
-        self.total_size = N
-        self.dim = dim
-        self.sd = sd 
-        self.scale = scale
-        np.random.seed(1024)
-        self.X_c = self.scale*np.random.normal(0, self.sd**2, (self.total_size,self.dim))
-        #self.X_c = self.scale*np.random.uniform(-1, 1, (self.total_size,self.dim))
-        self.label_idx = np.random.randint(low = 0 , high = self.nb_classes, size = self.total_size)
-        self.X_d = np.eye(self.nb_classes, dtype='float32')[self.label_idx]
-        self.X = np.hstack((self.X_c,self.X_d)).astype('float32')
-    
-    def train(self,batch_size,weights=None):
-        X_batch_c = self.scale*np.random.normal(0, 1, (batch_size,self.dim)).astype('float32')
-        #X_batch_c = self.scale*np.random.uniform(-1, 1, (batch_size,self.dim))
-        if weights is None:
-            weights = np.ones(self.nb_classes, dtype=np.float64) / float(self.nb_classes)
-        label_batch_idx =  np.random.choice(self.nb_classes, size=batch_size, replace=True, p=weights)
-        X_batch_d = np.eye(self.nb_classes,dtype='float32')[label_batch_idx]
-        return X_batch_c, X_batch_d
+class Assump_valid_sampler(object):
+    def __init__(self, v_dim, z_dim, N=100000, n_heldout=10000, random_seed=123):
+        from scipy.stats import ortho_group
+        np.random.seed(random_seed)
+        self.sample_size = N
+        #preset diagonal matrix M
+        eigenvalues = np.hstack([np.linspace(5,4,10),np.linspace(0.1,0.01,v_dim-10)])
+        M = np.diag(eigenvalues)
+        PCA_recon = sum(eigenvalues[z_dim:])
+        print('PCA_reconstruction error: %.5f'%PCA_recon)
+        #randomly generating othornomal bases and generate V
+        U = ortho_group.rvs(v_dim)
+        self.Sigma = np.dot(np.dot(U,M), U.T)
+        self.mu = np.random.uniform(low=-1.0, high=1.0,size=(v_dim,))
+        V = np.random.multivariate_normal(mean=self.mu, cov=self.Sigma,size = self.sample_size)
+        V_heldout = np.random.multivariate_normal(mean=self.mu, cov=self.Sigma,size = n_heldout)
+        #construct features of V
+        self.A = np.dot(np.diag(eigenvalues**(-0.5)),U.T)
+        T = np.dot(self.A, (V-self.mu).T).T
+        T_heldout = np.dot(self.A, (V_heldout-self.mu).T).T
+        self.A = self.A.astype('float32')
+        self.mu = self.mu.astype('float32')
+        self.data_v = V.astype('float32')
+        self.data_t = T.astype('float32')
+        self.data_v_heldout = V_heldout.astype('float32')
+        self.data_t_heldout = T_heldout.astype('float32')
 
+    def train(self, batch_size):
+        indx = np.random.randint(low = 0, high = self.sample_size, size = batch_size)
+        return self.data_v[indx, :], self.data_t[indx, :]
     def load_all(self):
-        return self.X_c, self.X_d
+        return self.data_v, self.data_t
 
-#sample continuous (Gaussian Mixture) and discrete (Catagory) latent variables together
-class Mixture_sampler_v2(object):
-    def __init__(self, nb_classes, N, dim, weights=None,sd=0.5):
-        self.nb_classes = nb_classes
-        self.total_size = N
-        self.dim = dim
-        np.random.seed(1024)
-        if nb_classes<=dim:
-            self.mean = np.random.uniform(-5,5,size =(nb_classes, dim))
-            #self.mean = np.zeros((nb_classes,dim))
-            #self.mean[:,:nb_classes] = np.eye(nb_classes)
-        else:
-            if dim==2:
-                self.mean = np.array([(np.cos(2*np.pi*idx/float(self.nb_classes)),np.sin(2*np.pi*idx/float(self.nb_classes))) for idx in range(self.nb_classes)])
-            else:
-                self.mean = np.zeros((nb_classes,dim))
-                self.mean[:,:2] = np.array([(np.cos(2*np.pi*idx/float(self.nb_classes)),np.sin(2*np.pi*idx/float(self.nb_classes))) for idx in range(self.nb_classes)])
-        self.cov = [sd**2*np.eye(dim) for item in range(nb_classes)]
-        if weights is None:
-            weights = np.ones(self.nb_classes, dtype=np.float64) / float(self.nb_classes)
-        self.Y = np.random.choice(self.nb_classes, size=N, replace=True, p=weights)
-        self.X_c = np.array([np.random.multivariate_normal(mean=self.mean[i],cov=self.cov[i]) for i in self.Y],dtype='float64')
-        self.X_d = np.eye(self.nb_classes)[self.Y]
-        self.X = np.hstack((self.X_c,self.X_d))
 
-    def train(self, batch_size, label = False):
-        indx = np.random.randint(low = 0, high = self.total_size, size = batch_size)
-        if label:
-            return self.X_c[indx, :], self.X_d[indx, :], self.Y[indx, :]
-        else:
-            return self.X_c[indx, :], self.X_d[indx, :]
-
-    def get_batch(self,batch_size,weights=None):
-        if weights is None:
-            weights = np.ones(self.nb_classes, dtype=np.float64) / float(self.nb_classes)
-        label_batch_idx =  np.random.choice(self.nb_classes, size=batch_size, replace=True, p=weights)
-        return self.X_c[label_batch_idx, :], self.X_d[label_batch_idx, :]
-    def predict_onepoint(self,array):#return component index with max likelyhood
-        from scipy.stats import multivariate_normal
-        assert len(array) == self.dim
-        return np.argmax([multivariate_normal.pdf(array,self.mean[idx],self.cov[idx]) for idx in range(self.nb_classes)])
-
-    def predict_multipoints(self,arrays):
-        assert arrays.shape[-1] == self.dim
-        return map(self.predict_onepoint,arrays)
-    def load_all(self):
-        return self.X_c, self.X_d, self.label_idx
-    
 def parse_file(path, sep='\t', header = 0, normalize=True):
     assert os.path.exists(path)
     if path[-3:] == 'npz':
