709feac55a12049e0c184fd5b1e8d49686c10fc4
Qiao Liu <liuqiao@buaa.edu.cn>
2022-12-29 01:30:20 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,33 +41,9 @@ needs_sphinx = "1.7"
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.coverage",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.githubpages",
-    "sphinx_autodoc_typehints",
-    "nbsphinx",
-    "edit_on_github",
 ]
 
 
-# Generate the API documentation when building
-autosummary_generate = True
-napoleon_google_docstring = False
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_use_rtype = False
-napoleon_custom_sections = [("Params", "Parameters")]
-
-intersphinx_mapping = dict(
-    python=("https://docs.python.org/3", None),
-    anndata=("https://anndata.readthedocs.io/en/latest/", None),
-    scanpy=("https://scanpy.readthedocs.io/en/latest/", None),
-    cellrank=("https://cellrank.readthedocs.io/en/latest/", None),
-)
 
 
 # General information about the project.
@@ -76,16 +52,7 @@ author = "Qiao Liu"
 title = "A general causal inference framework by encoding generative modeling"
 copyright = f"{datetime.now():%Y}, {author}"
 
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-pygments_style = "sphinx"
-todo_include_todos = False
 
 
-# -- Options for HTML output ----------------------------------------------
 
 html_theme = "sphinx_rtd_theme"
-html_theme_options = dict(navigation_depth=1, titles_only=True)
-github_repo = "CausalEGM"
-
-source_suffix = [".rst", ".ipynb"]
-master_doc = "index"
