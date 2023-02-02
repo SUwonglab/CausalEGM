9d7de6b3cbb08fd5c0d1b2cff97a41aecb84dfd8
kimmo1019 <liuqiao@buaa.edu.cn>
2023-02-02 12:59:21 -0800

    version 0.2.4

--- src/CausalEGM/__init__.py
+++ src/CausalEGM/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.2.2'
+__version__ = '0.2.4'
 from .causalEGM import CausalEGM
-from .util import Base_sampler, Semi_acic_sampler
+from .util import Base_sampler, Semi_acic_sampler, Sim_Hirano_Imbens_sampler, Sim_Sun_sampler, Sim_Colangelo_sampler, Semi_Twins_sampler
