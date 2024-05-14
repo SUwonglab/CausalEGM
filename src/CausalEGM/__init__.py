65533d977142670c9179a08de00e66881ed317ea
kimmo1019 <liuqiao@buaa.edu.cn>
2024-05-14 15:22:31 -0700

    v0.4.0 fix z1&z2 bug

--- src/CausalEGM/__init__.py
+++ src/CausalEGM/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.3.4'
+__version__ = '0.4.0'
 from .causalEGM import CausalEGM, VariationalCausalEGM
 from .util import Base_sampler, Semi_acic_sampler, Sim_Hirano_Imbens_sampler, Sim_Sun_sampler, Sim_Colangelo_sampler, Semi_Twins_sampler
